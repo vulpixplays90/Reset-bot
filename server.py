3f40af1d5752c1c0d2779a92429139172edea300
vulpixplays90 <davschool86@gmail.com>
2025-06-01 13:17:08 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+from threading import Thread 
+from flask import Flask 
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive"
+
+def run_http_server():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run_http_server)
+    t.start()
